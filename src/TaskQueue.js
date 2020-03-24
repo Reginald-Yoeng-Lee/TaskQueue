4dd4da21006bd9629c9e25b188eb5610592647d0
Reginald <Reginald.lay.y@gmail.com>
2020-03-24 19:03:33 +0800

    Rename some name and finish the doc.

--- src/TaskQueue.js
+++ src/TaskQueue.js
@@ -1,4 +1,5 @@
 const endPromiseResolvers = Symbol('hidePromiseResolver');
+const targetSymbol = Symbol('target');
 
 export default class TaskQueue {
 
@@ -10,49 +11,57 @@ export default class TaskQueue {
     }
 
     /**
-     * 添加要显示的弹窗及显示动作
+     * Add task related to a target with priority. The target will hold an end promise of the task. And it can be called
+     * endTaskWithTarget(target) to notify the task is done later. Next task won't start until endTaskWithTarget() is
+     * called.
      *
-     * @param target <object> 要显示的弹窗
-     * @param triggerAction <function> 显示弹窗的动作方法
+     * @param target <object> An object for holding the end promise of the task.
+     * @param task <function> The job needs to run in order.
+     * @param priority <number> The task will be added into the queue before every tasks with smaller priorities.
      */
-    addTaskWithTarget(target, triggerAction, priority = 0) {
+    addTaskWithTarget(target, task, priority = 0) {
         if (!target || typeof target !== 'object' && typeof target !== 'function') {
             throw new Error(``)
         }
-        const hidePromise = new Promise(resolve => {
+        const endPromise = new Promise(resolve => {
             if (!target[endPromiseResolvers]) {
                 target[endPromiseResolvers] = [];
             }
-            target[endPromiseResolvers].push(resolve);
             this._insert(target[endPromiseResolvers], {resolve}, priority);
         });
-        this.addTask(triggerAction, hidePromise);
+        // keep the target object. Mark the resolver wrapper running while task started.
+        endPromise[targetSymbol] = target;
+        this.addTask(task, endPromise, priority);
     }
 
     /**
-     * 通知弹窗已关闭, 如果有下一需要显示的弹窗将会自动显示
+     * End a running task with a target holding the end promise of the task and start next task(if any).
      *
-     * @param popup object 已关闭的弹窗
+     * @param target <object> An object holding the end promise of the task needs to end. That is, the target added with
+     *                        the task.
      */
-    notifyPopupHide(popup: Object) {
-        if (Array.isArray(popup[endPromiseResolvers])) {
-            const resolver = popup[endPromiseResolvers].shift();
-            typeof resolver === 'function' && resolver();
+    endTaskWithTarget(target) {
+        if (Array.isArray(target[endPromiseResolvers])) {
+            const resolver = target[endPromiseResolvers].shift();
+            resolver && typeof resolver.resolve === 'function' && resolver.resolve();
         }
     }
 
     /**
-     * 添加要显示的弹窗动作
+     * Add task running one by one. Only if the endPromise passed resolved, or the task is done (if it returns promise
+     * then until the promise resolved, else until the task returned), the task is considered finished and next task starts.
      *
-     * @param triggerAction function 显示弹窗的动作方法
-     * @param endPromise Promise | null 弹窗已隐藏的结果, 如果为空, 则检查triggerAction() 的返回结果, 如果结果为Promise 对象, 则使用返回结果替代hidePromise, 否则视为弹窗显示后立即关闭(通常为错误用法)
+     * @param task <function> Action needs to execute in order. If the endPromise isn't a Promise, then the task won't
+     *                        finish until the promise returned from the task function is resolved if it returns a promise or the task function returned.
+     * @param endPromise <Promise | null> If it's not null, the task will be finished while the promise resolved.
+     * @param priority <number> The task will be added into the queue before every tasks with smaller priorities.
      */
-    addTask(triggerAction, endPromise = null, priority = 0) {
-        if (typeof triggerAction !== 'function') {
-            throw new Error(`invalid trigger action type ${typeof triggerAction}.`);
+    addTask(task, endPromise = null, priority = 0) {
+        if (typeof task !== 'function') {
+            throw new Error(`invalid trigger action type ${typeof task}.`);
         }
 
-        this.tasks.push({triggerAction, endPromise});
+        this._insert(this.tasks, {task, endPromise}, priority);
 
         if (!this._running) {
             this._run();
@@ -73,10 +82,15 @@ export default class TaskQueue {
         }
 
         const taskWrapper = this.tasks.shift();
-        const result = taskWrapper.triggerAction();
+        const result = taskWrapper.task();
         let promise = null;
         if (taskWrapper.endPromise instanceof Promise) {
             promise = taskWrapper.endPromise;
+            // Find the end promise corresponding to this task and mark as running. Prevent another end promise of higher priority task adding before current end promise.
+            if (promise[targetSymbol] && Array.isArray(promise[targetSymbol][endPromiseResolvers]) && promise[targetSymbol][endPromiseResolvers] > 0) {
+                const wrapper = promise[targetSymbol][endPromiseResolvers][0];
+                wrapper._running = true;
+            }
         } else if (result instanceof Promise) {
             promise = result;
         }
@@ -89,14 +103,14 @@ export default class TaskQueue {
         this._run();
     }
 
-    _insert(queue, targetWrapper, priority) {
-        for (let [i, wrapper] of queue) {
-            if (wrapper.priority < priority) {
-                queue.splice(i, 0, {...targetWrapper, priority});
+    _insert(queue, originItem, priority) {
+        for (let [i, wrapper] of queue.entries()) {
+            if (!wrapper._running && wrapper.priority < priority) { // Only add after running wrapper.
+                queue.splice(i, 0, {...originItem, priority});
                 return;
             }
         }
-        queue.push({...targetWrapper, priority});
+        queue.push({...originItem, priority});
     }
 
     pause() {
